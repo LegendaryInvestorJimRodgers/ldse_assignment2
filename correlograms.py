ce15b3c2401937ea42f813e4bcabe0b933ad7941
LegendaryInvestorJimRodgers <nikita.granger@student.uva.nl>
2017-10-07 17:03:47 +0200

    correlogram and forecasts

---
+++ correlograms.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import scipy as sp
+from arch import arch_model
+import datetime as dt
+import matplotlib.pyplot as plt
+
+brentprices = pd.DataFrame.from_csv('BrentData.csv')
+wtiprices = pd.DataFrame.from_csv('WTIData.csv')
+data_agg = pd.DataFrame.from_csv('data_agg_full.csv')
+
+
+#REMEMBER: comment out this section when you run the regression
+brent_returns = 100 * brentprices['Returns']
+correlation_matrices = []
+correlogram_speed = []
+correlogram_slow = []
+correlogram_fast = []
+#correlogram
+for offset in range(-30, 0):
+    temp = data_agg.sort_index().shift(periods = offset)
+    data = pd.concat([brent_returns, temp], axis=1, join_axes=[data_agg.index])
+    correlation_matrices.append(np.asarray(data.corr()))
+
+for index in range(0, 30):
+    correlogram_speed.append(correlation_matrices[index][0, 1])
+    correlogram_slow.append(correlation_matrices[index][0, 2])
+    correlogram_fast.append(correlation_matrices[index][0, 3])
--- garch.py
+++ garch.py
@@ -8,14 +8,24 @@ import matplotlib.pyplot as plt
 
 brentprices = pd.DataFrame.from_csv('BrentData.csv')
 wtiprices = pd.DataFrame.from_csv('WTIData.csv')
+data_agg = pd.DataFrame.from_csv('data_agg_full.csv')
+data_agg = pd.concat([data_agg, data_agg['slow_count'] + data_agg['fast_count']], axis = 1)
+# data_agg = pd.concat([data_agg, data_agg['slow_count'] / (data_agg['slow_count'] + data_agg['fast_count'])], axis = 1)
+data_agg = data_agg.rename(columns={ data_agg.columns[3]: "total_count" })
+data_agg = data_agg.sort_index().shift(periods = -2)
 
 brent_returns = 100 * brentprices['Returns']
+data = pd.concat([brent_returns, data_agg], axis=1, join_axes=[data_agg.index])
+data = data.dropna(axis =0)
+#include this for the forecats with variables
+# x = data[[ 'avg(avg(sog))', 'slow_count', 'fast_count']],
+am = arch_model( y = data['Returns'],  mean = 'ARX', lags = 1, vol = 'EGARCH', p = 1, o = 1, q = 1, power = 2, dist = 'Normal')
 
-am = arch_model(brent_returns, mean = 'ARX', lags = 2, vol = 'EGARCH', p = 1, o = 1, q = 1, power = 2, dist = 'Normal')
-
-split_date = dt.datetime(2014,1,1)
+split_date = dt.datetime(2015,6,1)
 res = am.fit(last_obs = split_date)
 print(res.summary())
+
+
 forecasts = res.forecast(horizon = 4, start = split_date, method='bootstrap')
 sims = forecasts.simulations
 lines = plt.plot(sims.residual_variances[-1].T, color='#9cb2d6')
