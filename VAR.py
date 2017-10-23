5f833cc7ed05438d0e0bcabd7f50999834c6b0f3
LegendaryInvestorJimRodgers <nikita.granger@student.uva.nl>
2017-10-23 15:48:38 +0200

    with fast and slow counts

--- VAR.py
+++ VAR.py
@@ -6,22 +6,36 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from statsmodels.tsa.api import VAR
+from statsmodels.tsa.stattools import acf, pacf
 
 brentprices = pd.DataFrame.from_csv('BrentData.csv')
-brentprices = brentprices.shift(periods = -3)
+brentprices = brentprices#.shift(periods = -3)
 wtiprices = pd.DataFrame.from_csv('WTIData.csv')
-data_agg = pd.DataFrame.from_csv('data_agg_full.csv')
+data_agg = pd.DataFrame.from_csv('data_agg_w2017.csv')
 data_agg = data_agg.sort_index()#.shift(periods = -2)
 
 brent_returns = 100 * brentprices['Returns']
-brent_prices = brentprices['Adjusted Price']#.shift(periods = -1)
-data = pd.concat([brent_prices, data_agg], axis=1, join_axes=[data_agg.index])
+data_agg = data_agg[['fast_count']]
+# brent_prices = brentprices['Adjusted Price']#.shift(periods = -1)
+data = pd.concat([brentprices, data_agg], axis=1, join_axes=[data_agg.index])
 data = data.dropna(axis =0)
 
-model = VAR(data[['Adjusted Price', 'slow_count', 'fast_count', 'avg(avg(sog))']])
-res = model.fit(1)
-res.plot_acorr()
-plt.xlabel('August Price')
-plt.ylabel('Slow Count')
-plt.show()
+# #autocorrelation and partial autocorrelation for the prices
+# plt.plot(acf(data[['Adjusted Price']]))
+# plt.plot(pacf(data[['Adjusted Price']]))
+# plt.show()
+#
+# #autocorrelation and partial autocorrelaition for slow_count
+# plt.plot(acf(data[['slow_count']]))
+# plt.plot(pacf(data[['slow_count']]))
+# plt.show()
+#
+# #autocorrelation and partial autocorrelation for fast_count
+# plt.plot(acf(data[['fast_count']]))
+# plt.plot(pacf(data[['fast_count']]))
+# plt.show()
+
+model = VAR(data[['Adjusted Price',  'fast_count']])
+print(acf(brentprices[['Adjusted Price']]))
+res = model.fit()
 print(res.summary())
--- garch.py
+++ garch.py
@@ -10,17 +10,17 @@ from arch.univariate import ARX, EGARCH, base
 #1. GARCH Model for slow count
 #a. getting data and putting it into different variables
 brentprices = pd.DataFrame.from_csv('BrentData.csv')
-data_agg = pd.DataFrame.from_csv('data_agg_full.csv')
+data_agg = pd.DataFrame.from_csv('data_agg_w2017.csv')
 data_agg = data_agg.sort_index()
-slow_count = data_agg[['slow_count']]
-fast_count = data_agg[['fast_count']].shift(periods = -2)
+slow_count = data_agg[['slow_count']].shift(periods = -1)
+fast_count = data_agg[['fast_count']]
 avg_speed = data_agg[['avg(avg(sog))']].shift(periods = -1)
-brent_prices = brentprices['Adjusted Price'].shift(periods = -5)
+brent_prices = brentprices['Adjusted Price'].shift(periods = -3)
 data = pd.concat([brent_prices, avg_speed, slow_count, fast_count], axis=1, join_axes=[brent_prices.index])
 data = data.dropna(axis =0)
 
 #b. run the garch model for slow count
-am = arch_model(y = data[['slow_count']],x = data[['avg(avg(sog))', 'Adjusted Price', 'fast_count']],mean = 'ARX', lags = 1, vol = 'EGARCH', p = 1, o = 1, q = 1, power = 2, dist = 'Normal')
+am = arch_model(y = data[['fast_count']],x = data[['Adjusted Price', 'slow_count']],mean = 'ARX', lags = 1, vol = 'EGARCH', p = 1, o = 1, q = 1, power = 2, dist = 'Normal')
 res = am.fit()
 print(res.summary())
 
@@ -28,9 +28,9 @@ print(res.summary())
 data['intercept'] = np.ones(len(data))
 slow_count_lagged = slow_count.shift(periods = -1) #autoregressive term here
 data = pd.concat([slow_count_lagged, data], axis=1, join_axes = [slow_count.index])
-data.columns.values[0] = "slow_count_lagged"
+data.columns.values[0] = "fast_count_lagged"
 data = data.dropna(axis = 0)
-forecast = res.params[0] * data['intercept'] + res.params[1] * data['slow_count_lagged'] +  res.params[2] * data['avg(avg(sog))'] + res.params[3] * data['Adjusted Price'] + res.params[4] * data['fast_count']
+forecast = res.params[0] * data['intercept'] + res.params[1] * data['fast_count_lagged']  + res.params[2] * data['Adjusted Price'] + res.params[3] * data['slow_count']
 forecast = forecast.shift(periods = +1)
 
 #d. generate the forecasts for the volatility
@@ -38,9 +38,9 @@ volatility = data[['Adjusted Price']].rolling(80).var()
 volatility_data = pd.concat([volatility, res.resid], axis=1, join_axes = [volatility.index])
 volatility_data = volatility_data.dropna(axis = 0)
 e = volatility_data['resid'] / volatility_data['Adjusted Price']
-forecast_vol = np.exp(res.params[5] + res.params[6] * (abs(e) - np.sqrt(2/np.pi)) + res.params[7] * abs(e) + res.params[8] * np.log(volatility['Adjusted Price']))
+forecast_vol = np.exp(res.params[4] + res.params[5] * (abs(e) - np.sqrt(2/np.pi)) + res.params[6] * abs(e) + res.params[7] * np.log(volatility['Adjusted Price']))
 forecast_vol = forecast_vol.shift(periods = +1)
 
 forecast_final = pd.concat([forecast, np.sqrt(forecast_vol)], axis = 1, join_axes = [forecast.index])
 forecast_final = forecast_final.dropna(axis = 0)
-forecast_final.to_csv('forecast_final_slowcount.csv')
+forecast_final.to_csv('forecast_final_fastcount.csv')
